c5795af58cb01610ed21145a58cd7a01db017a5d
Pavel-Nefedov <81776498+Pavel-Nefedov@users.noreply.github.com>
2021-05-28 18:10:44 +0500

    Add files via upload
    Пытался сделать через With, не смог потерял много времени(((

---
+++ num_3.py
@@ -0,0 +1,16 @@
+from itertools import zip_longest
+
+hobby_f = open('hobby.csv', 'r', encoding='utf-8')
+user_f = open('users.csv', 'r', encoding='utf-8')
+out_f = open('out_file', 'w', encoding='utf=8')
+"""
+он записывал каждый раз на новой строке, чтобы убрать сделал Content и Clean_content
+они создают строку и убирают из нее лишние символы \n
+"""
+for user, hobby in zip_longest(user_f, hobby_f):
+        content = (f'{user}: {hobby}')
+        clean_content = content.replace('\n', ' ')# убирает лишние \n
+        out_f.write(f'{clean_content}\n')# используется уже для вывода каждого результата на новой строке
+hobby_f.close()
+user_f.close()
+out_f.close()
